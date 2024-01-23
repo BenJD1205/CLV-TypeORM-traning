a595abf466da2561d91f7750722dcf4f7a1256d3
BenJD1205 <s.anh2209@gmail.com>
2024-01-23 15:07:50 +0700

    chore: api-gateway and user-microservice

--- apps/api-gateway/src/app/auth/auth.controller.ts
+++ apps/api-gateway/src/app/auth/auth.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject, Post, Body, BadRequestException } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import {RegisterUserDto, LoginUserDto} from '@nest-orm/shared/dto'
+import {AuthCMD} from '@nest-orm/shared/command'
+import { catchError } from 'rxjs';
 
-@Controller('auth')
-export class AuthController {}
+@Controller({version:'1',path:'auth'})
+export class AuthController {
+    constructor(
+        @Inject('USER_MICROSERVICE') private readonly userService: ClientProxy
+    ){}
+
+    @Post('register')
+    async register(@Body() registerUserDto: RegisterUserDto):Promise<any>{
+        return this.userService.send({
+            cmd:AuthCMD.REGISTER,
+        },registerUserDto).pipe(catchError((err) => {
+            throw new BadRequestException(err);
+        }))
+    }
+
+    @Post('login')
+    async login(@Body() loginUserDto: LoginUserDto):Promise<any>{
+        return this.userService.send({
+            cmd: AuthCMD.LOGIN,
+        }, loginUserDto).pipe(catchError((err) => {
+            throw new BadRequestException(err);
+        }))
+    }
+}
